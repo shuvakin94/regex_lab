9199bc2cd3d678c898866475370eb0a938cfef6b
shuvakin94 <shuvakin94@gmail.com>
2015-05-20 11:29:19 +0300

    added reading of file

--- src/ru/miet/shuvakin/Analyze.java
+++ src/ru/miet/shuvakin/Analyze.java
@@ -1,7 +1,29 @@
 package ru.miet.shuvakin;
 
-/**
- * Created by Yura on 20.05.2015.
- */
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Analyze {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader;
+        List<String> lines;
+        reader = new BufferedReader(new InputStreamReader(new FileInputStream("myfile.txt"), Charset.defaultCharset()));
+        lines = new ArrayList<String>();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            lines.add(line);
+        }
+        String [] linesAsArray = lines.toArray(new String[lines.size()]);
+        StringBuffer text = new StringBuffer();
+        for (int i = 0; i < linesAsArray.length; i++) {
+            text.append(linesAsArray[i]);
+            text.append("\n");
+        }
+        System.out.println(text);
+    }
 }
